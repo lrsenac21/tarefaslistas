90822b2a16a23ef96a754f54d6d58964355f2ea4
TECNICO DE INFORMÁTICA 2021.2 <51127512021.2@senacrjedu.local>
2023-08-29 21:27:10 -0300

    tarefas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Tarefas } from './home.mode';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tarefas: Tarefas[] = [];
 
-  constructor() {}
+  constructor(private alerta: AlertController) { }
+
+  async showAdd() {
+    const screen = await this.alerta.create({
+      header: 'O que deseja fazer ?',
+      inputs: [
+        {
+          name: 'task',
+          type: 'text',
+          placeholder: 'O que deseja fazer ?'
+        }
+        
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: () =>{console.log('Excluido')}
+        },
+        {
+          text: 'Adicionar',
+          handler: ()=> {console.log('Incluir a tarefa')}
+        }
+
+      ]
+    });
+    screen.present();
+  }
 
 }
